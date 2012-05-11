5a444c991587dbaee1ed789e84cce945e684ff90
Jonathan Jones <jay6916@hotmail.co.uk>
2012-05-11 12:59:42 +0100

    Sound::Finished report and commented crowd

--- Sound/Coursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/HeaderFiles/Crowd.h
+++ Sound/Coursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/HeaderFiles/Crowd.h
@@ -1,30 +1,32 @@
 /*
-	Name	Crowd.h
-	Author	Jonathan Jones
-	Details	Conatins all the methods and variables that make up the crowd object
-	Purpose	To create a crowd sound
+	Name	:	Crowd.h
+	Author	:	Jonathan Jones
+	Details	:	Conatins all the methods and variables that make up the crowd object
+	Purpose	:	To create a crowd sound
 */
 
 #ifndef CROWD_H_
 #define CROWD_H_
 
+// Includes 
 #include <dsoundUtility.h>
 #include <EnvSound.h>
 #include <list>
 
+// Class definition
 class Crowd
 {
 public:
-	Crowd(DSoundDevice* DirectSoundDevice);
-	~Crowd();
-	void CreateNewSound(char* NameOfFile);
-	void SetPosition(const float x, const float y, const float z);
-	void Play();
-	void Stop();
+	Crowd(DSoundDevice* DirectSoundDevice);	// Constructor
+	~Crowd();	// Deconstuctor
+	void CreateNewSound(char* NameOfFile);	// Function that takes a name of a file to create a 3d sound
+	void SetPosition(const float x, const float y, const float z);	// Sets the position of the crowd
+	void Play();	// Play the crowd sound looping
+	void Stop();	// Stop the crowd sound
 private:
-	DSoundDevice* DirectSoundDevice_;
-	std::list<EnvSound*> CrowdList_;
-	float x_, y_, z_;
+	DSoundDevice* DirectSoundDevice_;	// Pointer to direct sound device
+	std::list<EnvSound*> CrowdList_;	// A list of all the environment sounds
+	float x_, y_, z_;					// The x, y, z position in 3D space of the crowd
 };
 
 #endif	// CROWD_H_
--- Sound/Coursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/SourceFiles/Crowd.cpp
+++ Sound/Coursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/AG0901_0901264_PorgrammingEnvironmentsCoursework/SourceFiles/Crowd.cpp
@@ -1,12 +1,16 @@
 /*
-	Name	Crowd.cpp
-	Author	Jonathan Jones
-	Details	Conatins all the methods and variables that make up the crowd object
-	Purpose	To create a crowd sound
+	Name	:	Crowd.h
+	Author	:	Jonathan Jones
+	Details	:	Conatins all the methods and variables that make up the crowd object
+	Purpose	:	To create a crowd sound
 */
 
+// Includes
 #include <Crowd.h>
 
+// Constructor
+// Set initial variables to zero
+// Sets direct sound point to the direct sound pointer
 Crowd::Crowd(DSoundDevice* DirectSoundDevice)
 : DirectSoundDevice_(DirectSoundDevice)
 , x_(0)
@@ -15,50 +19,73 @@ Crowd::Crowd(DSoundDevice* DirectSoundDevice)
 {
 }
 
+// Deconstuctor
+// Deletes all the sounds
 Crowd::~Crowd()
 {
-	// Release all sound files 
+	// Release all sound files
+	// Check to see if the crowd list is empty, if it isn't enter body
 	while(CrowdList_.empty())
 	{
+		// Delete the back object
 		delete CrowdList_.back();
 
+		// Pop the back oject
 		CrowdList_.pop_back();
 	}
 }
 
+// Function that takes a name of a file to create a 3d sound
 void Crowd::CreateNewSound(char* NameOfFile)
 {
+	// Create a variable to describe if sound buffer is created corretly
 	bool ok = true;
+
+	// Create a temporary sound pointer
 	Sound *SoundPointer;
+
+	// Create the new sound to check to see if it was created correctly
 	ok = ok && DirectSoundDevice_->CreateSoundBuffer(&SoundPointer, NameOfFile, DSBCAPS_CTRL3D);
 
+	// Create a new 3d sound and add it to the list
 	CrowdList_.push_back(new EnvSound(SoundPointer, x_, y_, z_));
 
+	// Set the sound pointer to zero to clean up
 	SoundPointer = 0;
 }
 
+// Sets the position of the crowd
+// Takes const values for x, y and z so that they cannot be changed accidently
 void Crowd::SetPosition(const float x, const float y, const float z)
 {
+	// Set the x, y and z position with passed variables
 	x_ = x;
 	y_ = y;
 	z_ = z;
 
+	// Loop through all the sounds in the list and set there new position
+	// For all items in the list
 	for(std::list<EnvSound*>::iterator iter = CrowdList_.begin(); iter != CrowdList_.end(); iter++)
 	{
+		// Set the new position
 		(*iter)->SetPosition(x_, y_, z_);
 	}
 }
 
+// Play the crowd sound looping
 void Crowd::Play()
 {
+	// Set all the sounds in the list to play continuously
 	for(std::list<EnvSound*>::iterator iter = CrowdList_.begin(); iter != CrowdList_.end(); iter++)
 	{
 		(*iter)->PlayLooping();
 	}
 }
 
+// Stop the crowd sound
 void Crowd::Stop()
 {
+	// Sets all the sounds in the list to stop playing
 	for(std::list<EnvSound*>::iterator iter = CrowdList_.begin(); iter != CrowdList_.end(); iter++)
 	{
 		(*iter)->StopPlaying();
